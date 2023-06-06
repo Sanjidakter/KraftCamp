37969ab6dcff890eeb3f852b120a7824cd447dfc
Sanjida Akter <sanjidakter2304@gmail.com>
2023-06-06 23:04:31 +0600

    navbar added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import Navbar from '../Shared/Navbar/Navbar';
+
+
+
+const Main = () => {
+    const location = useLocation();
+    console.log(location);
+    const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup')
+    return (
+        <div>
+            {/* {noHeaderFooter || <Navbar></Navbar>} */}
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+           {/* {noHeaderFooter || <Footer></Footer>} */}
+        </div>
+    );
+};
+
+export default Main;
