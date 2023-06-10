42475e09b5233960031eca728cc2554847fab64c
Sanjida Akter <sanjidakter2304@gmail.com>
2023-06-10 15:28:19 +0600

    toogle btn fr theme added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSpring, animated } from "react-spring";
+import Switch from "react-switch";
 import Banner from "../Banner/Banner";
 import Info from "../Info/Info";
 import PopularClassesSection from "../PopularClassesSection/PopularClassesSection";
 import MyComponent from "../MyComponent/MyComponent";
 import InstructorsSection from "../InstructorsSection/InstructorsSection";
-import { useSpring, animated } from "react-spring";
-import { useEffect } from "react";
-import { useState } from "react";
 
 const Home = () => {
   const animationProps = useSpring({
@@ -15,53 +14,63 @@ const Home = () => {
     config: { duration: 1000, delay: 500 },
   });
 
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
 
+  const handleToggle = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
-// for theme
-const [theme, setTheme] = useState(
-  localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-);
-
-  // update state on toggle
-const handleToggle = (e) => {
-  if (e.target.checked) {
-    setTheme("dark");
-  } else {
-    setTheme("light");
-  }
-};
-
-// set theme state in localstorage on mount & also update localstorage on state change
-useEffect(() => {
-  localStorage.setItem("theme", theme);
-  const localTheme = localStorage.getItem("theme");
-  // add custom data-theme attribute to html tag required to update theme using DaisyUI
-  document.querySelector("html").setAttribute("data-theme", localTheme);
-}, [theme]);
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
 
   return (
     <div>
       <section className="bg-yellow-200">
         <div className="container mx-auto py-8">
           <animated.h1
-            className="text-4xl font-bold text-center text-gray-900"
+            className={`text-4xl font-bold text-center ${
+              theme === "dark" ? "text-white" : "text-gray-900"
+            }`}
             style={animationProps}
           >
             KraftCamp
           </animated.h1>
           <animated.h2
-            className="text-2xl font-semibold text-center text-gray-700 mt-4"
+            className={`text-2xl font-semibold text-center ${
+              theme === "dark" ? "text-white" : "text-gray-700"
+            } mt-4`}
             style={animationProps}
           >
             Explore the Art of Crafting
           </animated.h2>
         </div>
       </section>
-      <Banner></Banner>
-      <Info></Info>
-      <PopularClassesSection></PopularClassesSection>
-      <MyComponent></MyComponent>
-      <InstructorsSection></InstructorsSection>
+
+      <div className="flex justify-center mt-4">
+        <Switch
+          checked={theme === "dark"}
+          onChange={handleToggle}
+          onColor="#86d3ff"
+          onHandleColor="#2693e6"
+          handleDiameter={24}
+          uncheckedIcon={false}
+          checkedIcon={false}
+          height={16}
+          width={48}
+          className="react-switch"
+        />
+      </div>
+
+      <Banner />
+      <Info />
+      <PopularClassesSection />
+      <MyComponent />
+      <InstructorsSection />
     </div>
   );
 };
